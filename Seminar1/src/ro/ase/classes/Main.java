c5d2ac53af31c26632ddb9e98ff35777e41e2b91
Stoica Stefan-Paul <73969392+Pauliche@users.noreply.github.com>
2022-05-12 01:34:17 +0300

    Add files via upload

---
+++ Seminar1/src/ro/ase/classes/Main.java
@@ -0,0 +1,14 @@
+package ro.ase.classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] v1= {-1, -1, 0, 0, 3};
+		int nrAp=0;
+		nrAp=Utils.nrMinAparitii(v1);
+		System.out.println(nrAp);
+		
+		
+	}
+
+}
